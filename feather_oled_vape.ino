d71beaff2dcd532d97854edf9dbf19238d05eafa
JackC <jciallella@gmail.com>
2017-05-14 23:19:40 -0700

    Working Battery :+1:
    The voltage reference was based on some tests I did using the LM4040
    Breakout showing that if adjusted slightly for this board, it will be be
    more accurate than the default 3.3v.

--- feather_oled_vape.ino
+++ feather_oled_vape.ino
@@ -85,21 +85,21 @@ Adafruit_SSD1306 display = Adafruit_SSD1306();
 #define battPin        A1       // Monitors Voltage
 
 /* Enable / Disable */
-#define VBAT_ENABLED    1       // Enable / Disable integrated batt mgmt
+#define VBAT_ENABLED  1         // Enable / Disable integrated batt mgmt
 
 /* Draws Battery Level in Icon */
-#define battStartX  4
-#define battStartY  25
-#define battWidth  8
+#define battStartX    4
+#define battStartY    25
+#define battWidth     8
 
 /* Draws Temp Level in Icon */
-#define tempStartX  53
-#define tempStartY  6
-#define tempWidth  2
+#define tempStartX    53
+#define tempStartY    6
+#define tempWidth     2
 #define tempMaxHeight 13
 
 /* Debounce / Button Hold */
-#define debounce 10             // prevent button noise
+#define debounce      10        // prevent button noise
 
 /* Temperature Variables (A) */
 int tButtonCount = 1;           // press counter
@@ -125,9 +125,9 @@ int fButtonPrev = 0;
 
 /* Battery Calculations */
 int battRead;                 // Mapped Voltage for LED
-int voltRead;                 // value is volts X 100, 5 vdc = 500
-int avgVoltRead = 0;          // the average used for power
-const int numReadings = 85;         // smooths readings
+int battSingleRead;           // Actual Voltage x1000
+int avgVoltRead = 0;          // the average used
+const int numReadings = 85;   // smooths voltage readings
 
 
 /************  SETUP  ************/
@@ -144,17 +144,18 @@ void setup()
   pinMode(mosfetPin,   OUTPUT);
   pinMode(vibeMotorPin,OUTPUT);
 
-  /* Pull-Up Resistors On/Off */
+  /* Resistors & References */
   // Nothing Here
   
   /* Setup Display */
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);     // Initialize I2C addr 0x3C (for 128x32)
+  
   // display.invertDisplay(true);                // White screen logo
+  // display.setRotation(2);                     // Rotate Display: 0, 90, 180 or 270* (0,1,2,3)
+  
   display.display();                             // Display splashscreen
   delay(1000);                                   // Time to display splash screen
   display.clearDisplay();                        // Clear buffer
-
-  avgVoltRead = voltRead; // OK Here?
 }
 
 
@@ -162,29 +163,24 @@ void setup()
 
 void loop()
 {
-  Serial.begin(9600);
-  Serial.println(battRead);
-  Serial.println();
-  Serial.println(avgVoltRead);
-  Serial.println();
-
-  // display.setRotation(2);  // Rotate Display: 0, 90, 180* or 270 degrees (0,1,2*,3)
+  Serial.begin(9600);  
 
   /* Battery Functions */
-  VoltRead();                 // Check Battery Level
-  Smooth();                   // Removes Jitter from Voltage Reads
-  LowBattery();               // Haptic notification to recharge
-  BattAdjust();               // Set Screen Readout
+  battSingleRead = getBatteryVoltage();         // Reads Voltage
+  Smooth();                                     // Removes Jitter from Voltage Reads
+  LowBattery();                                 // Haptic notification to recharge
+  BattAdjust();                                 // Set Screen Readout
+  Serial.println(avgVoltRead);
 
   /* Button Functions */
-  FireCoil();              // Heats Coil
-  ButtonReader();             // Check button presses
-  ResetCount();               // Reset button counters
-  
+  FireCoil();                                   // Heats Coil
+  ButtonReader();                               // Check button presses
+  ResetCount();                                 // Reset button counters
+
   /* Conditionally Display Main Menu */
   if (sButtonCount <= 1) {
     display.invertDisplay(false);
-    MainMenu();             // Render screen icons & text
+    MainMenu();               // Render screen icons & text
     TempAdjust();
   }
 
@@ -339,38 +335,24 @@ void ResetCount()
 }
 
 /* Read 1.1V reference against VCC for approx battery level */
-long VoltRead()
+int getBatteryVoltage()
 {
-      // set the reference to Vcc and the measurement to the internal 1.1V reference
-      #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-        ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-      #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
-        ADMUX = _BV(MUX5) | _BV(MUX0);
-      #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
-        ADMUX = _BV(MUX3) | _BV(MUX2);
-      #else
-        ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-      #endif  
-    
-      delay(2); // Wait for Vref to settle
-      ADCSRA |= _BV(ADSC); // Start conversion
-      while (bit_is_set(ADCSRA,ADSC)); // measuring
-    
-      uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
-      uint8_t high = ADCH; // unlocks both
-    
-      voltRead = (high<<8) | low;
-    
-      voltRead = 1125300L / voltRead; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
-      return voltRead; // Vcc in millivolts
-    
+    float measuredvbat = analogRead(battPin);
+
+    measuredvbat *= 2;        // we divided by 2, so multiply back
+    measuredvbat *= 3.228;    // 3.3V - Internal reference voltage
+    // measuredvbat *= 2.048; // External Reference LM4040 Breakout
+    measuredvbat /= 1024;     // convert to voltage
+    measuredvbat *= 1000;     // multiply by 1000 for range values
+
+    return measuredvbat;
 }
 
 /* Smooth Readings */
 void Smooth()
 {  
   for (int i = 0; i < numReadings; i++)  {
-    avgVoltRead = avgVoltRead + (voltRead - avgVoltRead) / numReadings;
+    avgVoltRead = avgVoltRead + (battSingleRead - avgVoltRead) / numReadings;
   }
   delay(1);
 }
@@ -378,8 +360,8 @@ void Smooth()
 /* Updates Battery Icon & Sets #*/
 void BattAdjust()
 {
-  battRead = map(avgVoltRead, 2750, 4250, 0, 99);
-  int battLines = map(battRead, 0, 95, 1, 17);
+  battRead = map(avgVoltRead, 2750, 4200, 0, 99);
+  int battLines = map(battRead, 0, 99, 1, 17);
   
   for (int i = 0; i <= battLines; i++)
   display.drawFastHLine(battStartX, battStartY - i, battWidth, 1);  
@@ -388,7 +370,7 @@ void BattAdjust()
 /* Haptic Feedback as Low Battery Warning */
 void LowBattery()
 {
-  if (voltRead < 2850)
+  if (avgVoltRead < 2850)
   {
     for (int i = 0; i <= 5; i++)
     Vibrate();
